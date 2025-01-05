656644e63519631541562d3814c9368116ed2fa9
vamsi-duvvana <135171346+vamsi-duvvana@users.noreply.github.com>
2025-01-05 17:05:16 +0530

    update in picking up port

--- main.py
+++ main.py
@@ -351,4 +351,5 @@ async def health_check():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
